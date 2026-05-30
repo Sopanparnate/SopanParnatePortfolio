9ce2a8a71bcc8e447ad2326af1f67eff1015210a
SOPAN GANESH PARNATE <parnatesopan6@gmail.com>
2026-05-30 09:23:46 +0530

    Update adminApi.js

--- frontend/src/adminApi.js
+++ frontend/src/adminApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const adminApi = axios.create({
-  baseURL: 'http://localhost:5000/api/admin',
+  baseURL: 'https://sopanparnateportfolio.onrender.com/api/admin',
   headers: { 'Content-Type': 'application/json' },
 });
 
